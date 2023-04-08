14015cbf9bd2ef36f1cfd1608d19f3aed0c2f0cf
Ismael Blandon <ismaelbm432@gmail.com>
2023-04-08 14:06:38 -0500

    better img

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
   return (
     <div className='container'>
         <div className='img_container'>
-            <img src="https://s3-alpha-sig.figma.com/img/ca59/d9ce/98042af437fdff212d3259040db2e2db?Expires=1680480000&Signature=Tr9zNQTb0fUhyJmZH9NP3qwVdfWVXIrkosGlSvI86Yig9zLhhR75thPor8ACsSctprhOn5NjOavtetDjvcXhQjI0A3uVW5CCUwjEqxD45lmyYGgI1yCca0zSZ1iMIDOMHX-XyM5uu4w5vMUVk4K3PgIyYUicJzpZlOENYBDxwM3M18PYVs6bEqSNsEksGRaHJEQEt3F8rwRPM2rb3E17ISCJv67bACWbCojPFH7~hCk7sLletlxbh6mll5meJ4VqQYtnBJ0llqIxY5D9a2jnhffEulOGh8AX2uPl3sWhez4jkBqsIDhYsecxs1wpasVpLHGPwfio0-lh83Dn4dDQNw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" width="400"/>
+            <img src="src/images/pokedex-logo.png" alt="" width="450" />
         </div>
         <div className='welcome_container'>
             <h1 className='hello'>¡Hello trainer!</h1>
